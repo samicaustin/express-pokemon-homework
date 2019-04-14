4713300cd497c38ca3f986f855c09475a78160ee
Sami Austin <samicaustin@gmail.com>
2019-04-14 16:27:56 -0600

    Commit 1 - Server is set up and running.

---
+++ server.js
@@ -0,0 +1,25 @@
+// SERVER SETUP
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// PORT LISTENER
+const port = 3000;
+app.listen(port, function() {
+    console.log("App is runnin', runnin', and runnin', runnin'")
+})
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// DATA FILE
+const pokemon = require('./pokemon.js');
+
+// INDEX ROUTE
+
+
+
+
+
+module.exports = app;
